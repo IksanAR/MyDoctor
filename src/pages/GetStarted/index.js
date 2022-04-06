aa4ac517886b0fe5da3bcaf3d59e6ff088241ddd
IksanAR <iksanabdulrohman@gmail.com>
2022-04-06 15:39:26 +0700

    add splash and getstarted page

---
+++ src/pages/GetStarted/index.js
@@ -0,0 +1,42 @@
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import React from 'react';
+import {ILLogo, ILGetStarted} from '../../assets';
+import {Button, Gap} from '../../components';
+
+const GetStarted = ({navigation}) => {
+  return (
+    <ImageBackground source={ILGetStarted} style={styles.page}>
+      <View>
+        <ILLogo />
+        <Text style={styles.title}>
+          Konsultasi dengan dokter jadi lebih mudah & fleksibel
+        </Text>
+      </View>
+      <View>
+        <Button
+          title="Get Started"
+          onPress={() => navigation.navigate('Register')}
+        />
+        <Gap height={16} />
+        <Button type="secondary" title="Sign In" />
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default GetStarted;
+
+const styles = StyleSheet.create({
+  page: {
+    padding: 40,
+    justifyContent: 'space-between',
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  title: {
+    fontSize: 28,
+    color: 'white',
+    marginTop: 91,
+    fontFamily: 'Nunito-SemiBold',
+  },
+});
